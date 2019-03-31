1ede64e42b50b371f7a7523e3c94c5fd3725e7ac
Shi Tian <shitian007@hotmail.com>
2019-03-31 15:19:17 +0800

    Setup basic models

--- src/Models/Main.elm
+++ src/Models/Main.elm
@@ -1,4 +1,43 @@
-module Models.Main exposing (..)
+module Models.Main exposing (Color(..), Model, Msg(..), PieceType(..), init)
+
+import Dict exposing (Dict)
+
+
+type PieceType
+    = Rook
+    | Knight
+    | Bishop
+    | King
+    | Queen
+    | Pawn
+
+
+validPos : Position -> Maybe Position
+validPos (x, y) =
+  if x >= 0 && x <= 8 && y >= 0 && y <= 8 then
+    Just (x, y)
+  else
+    Nothing
+
+
+type alias Position =
+    ( Int, Int )
+
+
+type Color
+    = White
+    | Black
+
+
+type alias Piece =
+    { color : Color
+    , pieceType : PieceType
+    }
+
+
+type alias ChessBoard =
+    Dict Position Piece
+
 
 type Msg
     = NoOp
@@ -13,3 +52,5 @@ init =
     ( {}, Cmd.none )
 
 
+
+--
